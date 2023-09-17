b1cc974feb08ca60699e60dbddd9a9f733a8e54a
Kayne Paranihi <14kayne12@gmail.com>
2023-09-18 11:21:42 +1200

    (WIP) begin basic styling for dashboard

---
+++ client/components/Dashboard.tsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+
+function Dashboard() {
+  return (
+    <>
+      <header>JOB</header>
+      <h1>h1 tag</h1>
+    </>
+  )
+}
+
+export default Dashboard
